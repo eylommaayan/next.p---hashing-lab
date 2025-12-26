b11ee79cdc210fe87c064a78368747e8a1e5affc
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2025-12-26 17:03:30 +0200

    עיצוב

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,28 +12,6 @@ export default function Home() {
 
   const [fileHash, setFileHash] = useState("");
 
-  async function register() {
-    setMsg("");
-    const res = await fetch("/api/auth/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password: pass }),
-    });
-    const data = await res.json();
-    setMsg(res.ok ? "Registered ✅" : `Error: ${data.error}`);
-  }
-
-  async function login() {
-    setMsg("");
-    const res = await fetch("/api/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password: pass }),
-    });
-    const data = await res.json();
-    setMsg(res.ok ? "Logged in ✅" : `Error: ${data.error}`);
-  }
-
   const hashText = async () => {
     try {
       const enc = new TextEncoder();
@@ -60,7 +38,9 @@ export default function Home() {
         fontFamily: "system-ui",
       }}
     >
-      <h1>Hashing Lab (Next.js + MongoDB)</h1>
+      <h1 className="text-4xl font-bold text-center text-red-500">
+        Hashing Lab
+      </h1>
 
       <section
         style={{
@@ -70,24 +50,6 @@ export default function Home() {
           borderRadius: 12,
         }}
       >
-        <h2>1) Password Hashing (bcrypt)</h2>
-        <input
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          placeholder="password"
-          type="password"
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
-        />
-        <div style={{ marginTop: 8 }}>
-          <button onClick={register}>Register</button>
-          <button onClick={login} style={{ marginLeft: 8 }}>
-            Login
-          </button>
-        </div>
         <p>{msg}</p>
       </section>
 
@@ -110,22 +72,6 @@ export default function Home() {
           Hash
         </button>
         <p style={{ wordBreak: "break-all" }}>{textHash}</p>
-      </section>
-
-      <section
-        style={{
-          marginTop: 24,
-          padding: 16,
-          border: "1px solid #ddd",
-          borderRadius: 12,
-        }}
-      >
-        <h2>3) File Integrity (SHA-256)</h2>
-        <input
-          type="file"
-          onChange={(e) => e.target.files?.[0] && hashFile(e.target.files[0])}
-        />
-        <p style={{ wordBreak: "break-all" }}>{fileHash}</p>
       </section>
     </main>
   );
